9110a87e31cb8df5679c511687f37482631700cf
Nidhi Sharma <49151319+Nidhi786sharma@users.noreply.github.com>
2020-08-14 14:51:57 +0530

    AND gate

---
+++ and.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:10:12 06/20/2019 
+// Design Name: 
+// Module Name:    and 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module and1(
+    input a,
+    input b,
+    output out
+    );
+ and a1 (out, a,b);
+
+endmodule
